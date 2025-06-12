4646afaf76fc25aa4f807b991c333d66a806bb2e
j2214 <j2214@matsue-ct.ac.jp>
2025-06-12 05:23:21 +0000

    11章完了

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+let todos = [];
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'ToDo App',
+    todos: todos,
+  });
+});
+
+router.post('/', function (req, res, next) {
+  const todo = req.body.add;
+  todos.push(todo);
+  res.redirect('/');
 });
 
 module.exports = router;
