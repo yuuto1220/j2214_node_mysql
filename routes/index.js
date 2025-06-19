4f22df2e00dd314caf2b60da4d6a4d7419fbff1c
j2214 <j2214@matsue-ct.ac.jp>
2025-06-19 05:34:32 +0000

    14章完了

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('../db/knex');
+const mysql = require('mysql');
 
-let todos = [];
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'todo_app'
+});
 
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'ToDo App',
-    todos: todos,
-  });
+  knex("tasks")
+    .select("*")
+    .then(function (results) {
+      console.log(results);
+      res.render('index', {
+        title: 'ToDo App',
+        todos: results,
+      });
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+      });
+    });
 });
 
 router.post('/', function (req, res, next) {
   const todo = req.body.add;
-  todos.push(todo);
-  res.redirect('/');
+  knex("tasks")
+    .insert({user_id: 1, content: todo})
+    .then(function () {
+      res.redirect('/')
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+      });
+    });
 });
 
 module.exports = router;
